ebf4c2f584ebee130c6638df9ee4f063fd8d261c
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-09-04 12:03:18 +0100

    Packages created

--- src/main/java/com/example/bankrestapi/model/Users.java
+++ src/main/java/com/example/bankrestapi/model/Users.java
@@ -1,7 +1,17 @@
 package com.example.bankrestapi.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
 }
